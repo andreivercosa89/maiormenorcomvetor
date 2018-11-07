4f53c072930fa67c5ff954d0de3bd92c7ed9a131
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-06 21:34:19 -0400

    testar maior e menor do vetor

---
+++ maiormenorcomvetor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace maiormenorcomvetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = 0, menor = 999999;
+
+            Console.WriteLine("Informe uma quantidade: ");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int [quantidade];
+
+            for (int i = 0; i < quantidade;i++){
+                Console.WriteLine("Informe um numero: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < quantidade; i++)
+            {
+
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+                else if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                }
+
+            }
+
+            Console.Write("Maior: {0}\nMenor: {1}",maior, menor);
+
+        }
+    }
+}
